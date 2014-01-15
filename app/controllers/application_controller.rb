055aa6a4e297731ced2080030a2b0c1c702ef4cf
Anniebunnie <aj.kim84@gmail.com>
2014-01-14 20:43:42 -0500

    heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,4 @@ class ApplicationController < ActionController::Base
 
   before_filter :authenticate_user!
 
-    MusixMatch::API::Base.api_key = ENV['MUSIXMATCH_KEY']
-
- #    def authenticate
-
-	#   root URL = http://api.musixmatch.com/ws/1.1/track.snippet.get?track_id=16860631
-	#   api_key = 169a79c608c2cf7a673fec03dcb9af37
-
-	# end
 end
